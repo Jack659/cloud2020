eaa05fdb8e6ef403d1cc0cc16cbca563c03159a9
Jack <851359901@qq.com>
2020-05-12 23:32:08 +0800

    添加脑图; feign;

---
+++ cloud-consumer-feign-order80/src/main/java/com/jack/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,30 @@
+package com.jack.springcloud.controller;
+
+import com.jack.springcloud.entities.CommonResult;
+import com.jack.springcloud.entities.Payment;
+import com.jack.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Jack
+ * @Date: 2020/5/12 23:23
+ */
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/jack/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,21 @@
+package com.jack.springcloud.service;
+
+import com.jack.springcloud.entities.CommonResult;
+import com.jack.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author: Jack
+ * @Date: 2020/5/12 23:15
+ */
+@Component
+@FeignClient("CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
